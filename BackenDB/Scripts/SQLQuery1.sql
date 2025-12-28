87df60039c067b86116693171af7887bade8a669
Esolano <daveysolano124@gmail.com>
2025-12-27 21:51:39 -0500

    DEV/Esolano
    Iniciar creacion de DB y tablas

---
+++ BackenDB/Scripts/SQLQuery1.sql
@@ -0,0 +1,24 @@
+/******************************************************
+*     FECHA: 27 de Diciembre 2025          
+*     AUTOR: Erneys Solano
+*     ACCION: Crear tablas
+*     
+*     DESCRIPCION:
+*     Con la finalidad de llevar a cabo un proyecto 
+*     personal sobre un aplicacion de gestion de 
+*     nomtas academics, registro de estudiantes,
+*     y otras acciones mas, el cual se iran agraga-
+*     ndo con el tempo le iniciare
+*
+******************************************************
+*/
+
+
+
+
+
+
+
+use DBColegio
+go
+
