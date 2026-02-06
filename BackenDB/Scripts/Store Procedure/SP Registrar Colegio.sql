756d4f717ab6941511558464a166a9b7b57e68cd
Esolano <daveysolano124@gmail.com>
2026-02-05 22:20:47 -0500

    Esolano/DEV feat: Se actualiza y se crea
    Se actualiza el procedimiento almacenado Guardar y se crea el procedimiento buscar

--- BackenDB/Scripts/Store Procedure/SP Registrar Colegio.sql
+++ BackenDB/Scripts/Store Procedure/SP Registrar Colegio.sql
@@ -8,12 +8,21 @@
 *     registar el colegio
 *
 ******************************************************
+*     FECHA: 5-02-2026          
+*     AUTOR: Erneys Solano
+*     ACCION: alterar  SP registrar colegio
+*     
+*     DESCRIPCION:
+*     Se realiza actualizacion de sp registrar colegio
+*     con el fin de que valide si ya esta registrado el
+*     colegio
+******************************************************
 */
 
 use DBColegio
 go
 
-create procedure sp_registrar_colegio
+Alter procedure sp_registrar_colegio
 @nit bigint,
 @nombre varchar (20),
 @direccion varchar (50),
@@ -28,6 +37,12 @@ AS
 BEGIN
      SET NOCOUNT ON;
 
+     IF EXISTS (SELECT 1 FROM Tbl_Colegio WHERE nit = @nit)
+     BEGIN
+          RAISERROR('ya existe un registro con ese nit', 16, 1);
+          RETURN
+     END
+
      INSERT INTO Tbl_Colegio (nit, nombre,direccion,telefono,celular,correo,ciudad,fecha_registro,id_nivel_Academico,id_tipo,id_estado)
                        VALUES(@nit,@nombre,@direccion,@telefono,@celular,@correo,@ciudad,GETDATE(),@nivel_academico,@tipo_colegio,@estado)
-end                  
+END                  
