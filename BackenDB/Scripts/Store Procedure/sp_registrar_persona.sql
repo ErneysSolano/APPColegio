beef5830874601c4446429ae6609fe014eac7efd
Esolano <daveysolano124@gmail.com>
2026-07-13 21:43:25 -0500

    DEV/Esolano feat: se crea sp registrar persona, futuros cambios para cracion y asignacion tipo usuario

---
+++ BackenDB/Scripts/Store Procedure/sp_registrar_persona.sql
@@ -0,0 +1,53 @@
+/******************************************************
+*     FECHA: 13-07-2026          
+*     AUTOR: Erneys Solano
+*     ACCION: crear SP registrar persona
+*     DESCRIPCION:
+*     Se realiza procedimimeto alamcenado para 
+*     registar las personas en el sistema
+*
+******************************************************
+*/
+
+USE DBColegio
+GO
+
+ALTER PROCEDURE sp_registrar_persona
+@id_tipo_documento INT,
+@numero_documento BIGINT,
+@nombres VARCHAR(20),
+@apellidos VARCHAR(20),
+@celular VARCHAR(30),
+@correo VARCHAR(30),
+@id_tipo_sanguineo INT,
+@id_rol INT,
+@id_estado INT,
+@nit_colegio BIGINT,
+@fecha_nacimiento DATE
+AS
+BEGIN
+      SET NOCOUNT ON;
+
+      IF EXISTS (SELECT 1 FROM Tbl_Persona WHERE numero_documento =@numero_documento)
+      BEGIN
+           RAISERROR('[WARNING][MENSAJE ALERTA]YA EXISTE UN REGISTRO CON ESA CEDULA', 16,1);
+           RETURN;
+      END
+
+      DECLARE @edad INT;
+      SET @edad = DATEDIFF(YEAR, @fecha_nacimiento, GETDATE());
+
+      IF(MONTH(@fecha_nacimiento) > MONTH(GETDATE()))
+        OR (MONTH(@fecha_nacimiento) = MONTH(GETDATE()) AND DAY(@fecha_nacimiento) > DAY(GETDATE()))
+      BEGIN
+         SET @edad = @edad - 1;
+      END
+
+      INSERT INTO Tbl_Persona(
+                              id_tipo_Documento, numero_documento,nombres,apellidos,celular,correo,fecha_registro,id_tipo_sanguineo,
+                              id_rol,id_estado, nit_colegio, fecha_nacimiento, edad)
+                              VALUES(
+                              @id_tipo_documento, @numero_documento,@nombres,@apellidos,@celular,@correo, GETDATE(),@id_tipo_sanguineo,
+                              @id_rol,@id_estado,@nit_colegio, @fecha_nacimiento, @edad)
+
+END
